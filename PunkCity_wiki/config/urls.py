d6ded18bce4f42979e7e783bdb807e0cad688ab5
Rei <127863393+Arisuzea@users.noreply.github.com>
2026-05-23 08:31:11 +0800

    Added What's PunkCity page

--- PunkCity_wiki/config/urls.py
+++ PunkCity_wiki/config/urls.py
@@ -3,6 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    path('cyberpunk/', TemplateView.as_view(template_name='pages/cyberpunk.html'), name='cyberpunk'),
+    path('cyberpunk/', TemplateView.as_view(template_name='pages/basics/whatspunkcity.html'), name='whatspunkcity'),
     path('factions/', TemplateView.as_view(template_name='pages/factions.html'), name='factions'),
 ]
