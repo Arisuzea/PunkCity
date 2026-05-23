0151a18435a4b153503f41810affddc223e0764f
Rei <127863393+Arisuzea@users.noreply.github.com>
2026-05-23 11:53:25 +0800

    Added Vanilla vs PunkCity

--- PunkCity_wiki/config/urls.py
+++ PunkCity_wiki/config/urls.py
@@ -3,6 +3,10 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='pages/home.html'), name='home'),
-    path('cyberpunk/', TemplateView.as_view(template_name='pages/basics/whatspunkcity.html'), name='whatspunkcity'),
+    
+    # Updated paths below:
+    path('basics/whats-punk-city/', TemplateView.as_view(template_name='pages/basics/whatspunkcity.html'), name='whatspunkcity'),
+    path('basics/vanilla-vs-punk-city/', TemplateView.as_view(template_name='pages/basics/vanillavspunkcity.html'), name='vanillavspunkcity'),
+    
     path('factions/', TemplateView.as_view(template_name='pages/factions.html'), name='factions'),
 ]
